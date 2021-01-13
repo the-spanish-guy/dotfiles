b0b00aee3b2bc2683fc498953832a379f5917538
Luis Enrique Meza Paredes <luyo.lemp@gmail.com>
2021-01-13 20:14:31 -0300

    added script to install config of neovim

---
+++ nvim/install.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+DESTINATION_FOLDER=~/dotfiles_spanish
+
+installVimPlug() { \
+	echo "installing Vim-plug"
+
+	curl -fLo ~/.config/nvim/autoload/plug.vim --create-dirs \
+  	  https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+
+	echo "done"
+}
+
+cloneProject() { \
+	echo "cloning nvim configs"
+	git clone https://github.com/the-spanish-guy/dotfiles.git  ${DESTINATION_FOLDER}
+	mkdir ~/.config/nvim -p
+	cp -r ${DESTINATION_FOLDER}/nvim/lua ${DESTINATION_FOLDER}/nvim/settings ${DESTINATION_FOLDER}/nvim/coc-settings.json ${DESTINATION_FOLDER}/nvim/init.vim ~/.config/nvim
+
+	echo "removing repo"
+}
+
+installPynvim(){ \
+	python3 -m pip install --upgrade pip
+	python3 -m pip install pynvim
+}
+
+init() { \
+	echo "starting download"
+	installVimPlug
+	cloneProject
+	installPynvim	
+
+	rm -rf ${DESTINATION_FOLDER}
+	echo "configurations successfully installed"
+}
+
+init
+
